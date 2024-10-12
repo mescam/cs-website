e94193312adaed8469d245a8521b94ecbacb7af7
Jakub Woźniak <jakub.wozniak@nordcloud.com>
2024-10-12 22:38:34 +0200

    fix naming

--- labs/flake.nix
+++ labs/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Jakub Wozniak resume";
+  description = "Lab scripts for Distributed Systems Management at PUT";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -18,7 +18,7 @@
       in rec {
         packages = {
           document = pkgs.stdenvNoCC.mkDerivation rec {
-            name = "JakubWozniakResume";
+            name = "LaTeX-Build";
             src = self;
             buildInputs = [ pkgs.coreutils tex ];
             phases = [ "unpackPhase" "buildPhase" "installPhase" ];
