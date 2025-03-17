af02cb254a3648fc47de3ce397128ab2b08b8456
Jakub Woźniak <jakub.wozniak@nordcloud.com>
2025-03-17 14:51:00 +0100

    new lecture

---
+++ lectures-src/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Lab scripts for Distributed Systems Management at PUT";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    with flake-utils.lib;
+    eachSystem allSystems (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive)
+            scheme-full latex-bin latexmk curve fontawesome5 silence
+            simpleicons relsize comment biblatex csquotes cochineal xstring
+            cabin inconsolata upquote xurl;
+        };
+      in rec {
+        packages = {
+          document = pkgs.stdenvNoCC.mkDerivation rec {
+            name = "LaTeX-Build";
+            src = self;
+            buildInputs = [ pkgs.coreutils tex ];
+            phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+            buildPhase = ''
+              export PATH="${pkgs.lib.makeBinPath buildInputs}";
+              mkdir -p .cache/texmf-var
+              for i in *.tex; do
+                env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+                  latexmk -interaction=nonstopmode -pdf -xelatex -f \
+                  $i
+              done;
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp *.pdf $out/
+            '';
+          };
+        };
+        defaultPackage = packages.document;
+      });
+}
